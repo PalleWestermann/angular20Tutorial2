336834574f19c989331d7c40c8399ffa87fa50aa
Palle Westermann <pwe@tec.dk>
2025-12-21 15:43:09 +0100

    Afver video 5

--- src/app/components/control-flow/control-flow.ts
+++ src/app/components/control-flow/control-flow.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-control-flow',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './control-flow.html',
   styleUrl: './control-flow.css',
 })
 export class ControlFlow {
+
+  isParaVisible: boolean = true;
+
+  startMonthName: string = "feb";
+
+  cityList: string[] = ["Pune", "Mumbai", "Panji", "Nagpur"];
+
+  studentList: any [] = [
+    {name: 'AAAA', city:'Pune', isActive:false},
+    {name: 'BB', city:'Mumbai', isActive:false},
+    {name: 'CCC', city:'Pune', isActive:true},
+    {name: 'DDD', city:'Nagpur', isActive:false},
+  ];
+
+  hideP() {
+    this.isParaVisible = false;
+  }
+  showP() {
+    this.isParaVisible = true;
+  }
 
 }
